4885362558cb1c9800e432516bf9b9f100b48fa4
Mobin Yousefi <mobinyousefi.cs@gmail.com>
2025-11-22 11:44:30 +0330

    Add files via upload

---
+++ src/BareMinimumCodeNeeded/BareMinimumCodeNeeded.ino
@@ -0,0 +1,17 @@
+/*
+===========================================================
+ Project:    Bare Minimum Code Needed
+ File:       BareMinimumCodeNeeded
+ Author:     Mobin Yousefi (GitHub: github.com/mobinyousefi)
+ Created:    2025-11-22
+ Updated:    2025-11-22
+ License:    MIT License (see LICENSE file for details)
+===========================================================
+*/
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
